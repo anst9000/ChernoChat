320981d407ebf13f4443928473c7d367d126087e
Anders Strömberg <coltlaboy@yahoo.se>
2026-07-19 11:32:22 +0200

    Episode 7: Main Client Window

--- src/com/coltla/Client.java
+++ src/com/coltla/Client.java
@@ -2,16 +2,28 @@ package com.coltla;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.DefaultCaret;
+
 import java.awt.GridBagLayout;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.Font;
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
 
 public class Client extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	
+	private JTextArea history;
+	private DefaultCaret caret;
+	private JTextField txtMessage;
 
 	private String name;
 	private String address;
@@ -42,17 +54,54 @@ public class Client extends JFrame {
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[] { 16, 857, 7 }; 	// SUM = 880
+		gbl_contentPane.columnWidths = new int[] { 16, 827, 30, 7 }; 	// SUM = 880
 		gbl_contentPane.rowHeights = new int[] { 35, 475, 40 }; 		// SUM = 550
 		contentPane.setLayout(gbl_contentPane);
 		
-		JTextArea history = new JTextArea();
-		GridBagConstraints gbc_textArea = new GridBagConstraints();
-		gbc_textArea.fill = GridBagConstraints.BOTH;
-		gbc_textArea.gridx = 1;
-		gbc_textArea.gridy = 1;
-		contentPane.add(history, gbc_textArea);
+		history = new JTextArea();
+		history.setEditable(false);
+		history.setFont(new Font("Arial", Font.PLAIN, 14));
+		JScrollPane scroll = new JScrollPane(history);
+		caret = (DefaultCaret) history.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		GridBagConstraints scrollConstraints = new GridBagConstraints();
+		scrollConstraints.insets = new Insets(0, 0, 5, 5);
+		scrollConstraints.fill = GridBagConstraints.BOTH;
+		scrollConstraints.gridx = 0;
+		scrollConstraints.gridy = 0;
+		scrollConstraints.gridwidth = 3;
+		scrollConstraints.gridheight = 2;
+		scrollConstraints.weightx = 1;
+		scrollConstraints.weighty = 1;
+		scrollConstraints.insets = new Insets(0, 5, 0, 0);
+		contentPane.add(scroll, scrollConstraints);
+		
+		txtMessage = new JTextField();
+		txtMessage.setFont(new Font("Arial", Font.PLAIN, 14));
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 5, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 0;
+		gbc_txtMessage.gridy = 2;
+		gbc_txtMessage.gridwidth = 2;
+		gbc_txtMessage.weightx = 1;
+		gbc_txtMessage.weighty = 0;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.setFont(new Font("Arial", Font.PLAIN, 16));
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 0);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		gbc_btnSend.weightx = 0;
+		gbc_btnSend.weighty = 0;
+		contentPane.add(btnSend, gbc_btnSend);
+		
 		setVisible(true);
+		
+		txtMessage.requestFocusInWindow();
 	}
 
 }
--- src/com/coltla/Login.java
+++ src/com/coltla/Login.java
@@ -16,111 +16,92 @@ import java.awt.event.ActionEvent;
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+
 	private JPanel contentPane;
-
-	private JLabel lblName;
-	private JTextField txtName;
-
-	private JLabel lblIpAddress;
-	private JTextField txtAddress;
-
-	private JLabel lblPort;
-	private JTextField txtPort;
-
-	private JLabel lblExampleaddress;
-	private JLabel lblExampleport;
-
+	private JTextField textName;
+	private JTextField textIPAddress;
+	private JTextField textPort;
 	private JButton btnLogin;
 
-	/**
-	 * Create the frame.
-	 */
 	public Login() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300, 400);
+		setSize(300, 380);
 		setLocationRelativeTo(null);
-
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		txtName = new JTextField();
-		txtName.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtName.setBounds(59, 50, 165, 28);
-		contentPane.add(txtName);
-		txtName.setColumns(10);
-
-		lblName = new JLabel();
-		lblName.setText("Name:");
-		lblName.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblName.setBounds(118, 28, 48, 16);
+		
+		textName = new JTextField();
+		textName.setFont(new Font("Arial", Font.PLAIN, 14));
+		textName.setBounds(60, 41, 165, 30);
+		contentPane.add(textName);
+		textName.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setFont(new Font("Arial", Font.PLAIN, 16));
+		lblName.setBounds(116, 20, 53, 17);
 		contentPane.add(lblName);
-
-		lblIpAddress = new JLabel("IP Address:");
-		lblIpAddress.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblIpAddress.setBounds(101, 105, 81, 16);
-		contentPane.add(lblIpAddress);
-
-		txtAddress = new JTextField();
-		txtAddress.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtAddress.setColumns(10);
-		txtAddress.setBounds(59, 129, 165, 28);
-		contentPane.add(txtAddress);
-
-		lblPort = new JLabel("Port:");
-		lblPort.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblPort.setBounds(124, 187, 36, 16);
+		
+		JLabel lblIPAddress = new JLabel("IP Address");
+		lblIPAddress.setFont(new Font("Arial", Font.PLAIN, 16));
+		lblIPAddress.setBounds(99, 88, 88, 17);
+		contentPane.add(lblIPAddress);
+		
+		textIPAddress = new JTextField();
+		textIPAddress.setFont(new Font("Arial", Font.PLAIN, 14));
+		textIPAddress.setColumns(10);
+		textIPAddress.setBounds(60, 110, 165, 30);
+		contentPane.add(textIPAddress);
+		
+		JLabel lblPort = new JLabel("Port");
+		lblPort.setFont(new Font("Arial", Font.PLAIN, 16));
+		lblPort.setBounds(123, 170, 39, 17);
 		contentPane.add(lblPort);
-
-		txtPort = new JTextField();
-		txtPort.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtPort.setColumns(10);
-		txtPort.setBounds(59, 209, 165, 28);
-		contentPane.add(txtPort);
-
-		lblExampleaddress = new JLabel("(eg. 192.168.0.2)");
-		lblExampleaddress.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblExampleaddress.setBounds(89, 156, 105, 16);
-		contentPane.add(lblExampleaddress);
-
-		lblExampleport = new JLabel("(eg. 8192)");
-		lblExampleport.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblExampleport.setBounds(106, 237, 72, 16);
-		contentPane.add(lblExampleport);
-
+		
+		textPort = new JTextField();
+		textPort.setFont(new Font("Arial", Font.PLAIN, 14));
+		textPort.setColumns(10);
+		textPort.setBounds(60, 191, 165, 30);
+		contentPane.add(textPort);
+		
+		JLabel lblExampleIPAddress = new JLabel("eg. 192.168.0.2");
+		lblExampleIPAddress.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblExampleIPAddress.setBounds(87, 141, 112, 17);
+		contentPane.add(lblExampleIPAddress);
+		
+		JLabel lblExamplePort = new JLabel("eg. 8192");
+		lblExamplePort.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblExamplePort.setBounds(109, 221, 68, 17);
+		contentPane.add(lblExamplePort);
+		
 		btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String name = txtName.getText();
-				String address = txtAddress.getText();
-				int port = Integer.parseInt(txtPort.getText());
-				handleLogin(name, address, port);
+				String name = textName.getText();
+				String address = textIPAddress.getText();
+				int port = Integer.parseInt(textPort.getText());
+				
+				login(name, address, port);
 			}
 		});
-		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnLogin.setBounds(97, 312, 89, 23);
+		btnLogin.setFont(new Font("Arial", Font.PLAIN, 16));
+		btnLogin.setBounds(76, 272, 133, 30);
 		contentPane.add(btnLogin);
-
 	}
-
-	private void handleLogin(String name, String address, int port) {
-		// TODO: Check valid name, address and port 
+	
+	private void login(String name, String address, int port) {
 		dispose();
 		new Client(name, address, port);
 	}
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
