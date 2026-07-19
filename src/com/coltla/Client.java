32255f3de039f88af89f62a198b9f84e3eb56103
Anders Strömberg <coltlaboy@yahoo.se>
2026-07-19 10:19:41 +0200

    Episode 6: Gridbag Layout

--- src/com/coltla/Client.java
+++ src/com/coltla/Client.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
 
 public class Client extends JFrame {
 
@@ -33,9 +36,22 @@ public class Client extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(880, 550);
 		setLocationRelativeTo(null);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);	
+		setContentPane(contentPane);
+		
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 16, 857, 7 }; 	// SUM = 880
+		gbl_contentPane.rowHeights = new int[] { 35, 475, 40 }; 		// SUM = 550
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea history = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 1;
+		gbc_textArea.gridy = 1;
+		contentPane.add(history, gbc_textArea);
 		setVisible(true);
 	}
 
