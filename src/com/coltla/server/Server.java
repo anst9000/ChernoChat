7d415dd2e8f97af81799da1f94844a79fee9d54e
Anders Strömberg <coltlaboy@yahoo.se>
2026-07-19 20:56:00 +0200

    Episode 15: Server Threads and Explanations

--- src/com/coltla/server/Server.java
+++ src/com/coltla/server/Server.java
@@ -1,9 +1,58 @@
 package com.coltla.server;
 
-public class Server {
-  private int port;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server implements Runnable {
+	private DatagramSocket socket;
+	private int port;
+	
+	private boolean isRunning = false;
+	
+	private Thread serverThread;
+	private Thread manageThread;
+	private Thread sendThread;
+	private Thread receiveThread;
 
   public Server(int port) {
     this.port = port;
+    
+    try {
+		socket = new DatagramSocket(port);
+	} catch (SocketException soex) {
+		soex.printStackTrace();
+	}
+    
+    serverThread = new Thread(this, "Server Thread");
   }
+
+	public void run() {
+		isRunning = true;
+		manageClients();
+		receive();
+	}
+	
+	private void manageClients() {
+		manageThread = new Thread("Manage Thread") {
+			public void run() {
+				while (isRunning) {
+					// Managing Clients
+				}
+			}
+		};
+		
+		manageThread.start();
+	}
+
+	private void receive() {
+		receiveThread = new Thread("Receive Thread") {
+			public void run() {
+				while (isRunning) {
+					// Receive Data
+				}
+			}
+		};
+		
+		receiveThread.start();
+	}
 }
